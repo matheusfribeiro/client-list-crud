8584d0c07ff4f3d167cd5c6341690e34995ea97e
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-03-10 09:26:19 -0300

    feat: modal box - add client

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@ const modal = document.querySelector('#modal')
 addButton.addEventListener('click', () => {
   modal.style.display = 'block'
 })
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
