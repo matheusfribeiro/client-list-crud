914fa449585ce9500aefb851a62b4d37ebd53fcf
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-03-14 21:30:03 -0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,82 @@
-const addButton = document.querySelector('#addButton')
-const modal = document.querySelector('#modal')
-const saveButton = document.querySelector('#saveButton')
-const table = document.querySelector('#table')
-const companyInput = document.querySelector('#companyName')
-const contactInput = document.querySelector('#contactName')
-const countryInput = document.querySelector('#countryName')
-const inputs = document.querySelectorAll('input')
-const deleteButton = document.getElementById('deleteButton')
+
+
+const Main = {
+  init: function () {
+    this.cacheSelectors()
+    this.bindEvents()
+    
+  },
+
+  cacheSelectors: function () {
+    this.addButton = document.querySelector('#addButton')
+    this.modal = document.querySelector('#modal')
+    this.saveButton = document.querySelector('#saveButton')
+    this.table = document.querySelector('#table')
+    this.companyInput = document.querySelector('#companyName')
+    this.contactInput = document.querySelector('#contactName')
+    this.countryInput = document.querySelector('#countryName')
+    this.inputs = document.querySelectorAll('input')
+    this.clients = []
+  },
+
+  bindEvents: function () {
+    this.addButton.addEventListener('click', this.Events.addButton_click.bind(this))
+    window.onclick = this.Events.closeModal_click
+    this.saveButton.addEventListener('click', this.Events.saveClient_click.bind(this))
+  },
+
+
+  Events: {
+    addButton_click:function () {
+      modal.style.display = 'block'
+      this.inputs.forEach(input => {
+        input.value = ''
+      })
+    },
+
+    closeModal_click: function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    },
+
+    
+
+    saveClient_click: function () {
+      
+      
+      companyName = this.companyInput.value
+      contactName = this.contactInput.value
+      countryName = this.countryInput.value
+
+      /*
+      const addClient = {
+        id: (clients.length + 1),
+        company: companyName,
+        contact: contactName,
+        country: countryName
+      }
+
+      clients.push(addClient)
+      */
+      console.log(this.clients)
+      modal.style.display = "none";
+
+    }
+  }
+
+
+}
+
+Main.init()
+
+/*
+
 
 
 
 addButton.addEventListener('click', () => {
-  modal.style.display = 'block'
-  inputs.forEach(input => input.value = '')
+  this.inputs.forEach(input => input.value = '')
 })
 
 // when user clicks outside de modal, it closes
@@ -24,13 +88,46 @@ window.onclick = function(event) {
 
 
 
+function displayClients () {
+  table.innerHTML = `
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+          <th>Edit | Delete</th>
+        </tr>
+        <thead>  
+      `
+  var setClients = clients.map((client, i) => {
+    return `
+          
+      <tbody>
+        <tr>
+          <td>${client.id}</td>
+          <td>${client.company}</td>
+          <td>${client.contact}</td>
+          <td>${client.country}</td>
+          <td><i id="editButton" class="fa-solid fa-pen"></i><i id="deleteButton" class="fa-solid fa-trash"></i></td>
+        </tr>
+      </tbody>
+    `
+    
+  })
+
+  table.innerHTML += setClients.join("")
+  modal.style.display = "none";
+}
+
+
 let clients = [
   
 ]
 
+saveButton.addEventListener('click', saveClient) 
 
-
-saveButton.addEventListener('click', () => {
+function saveClient() {
   
 
   companyName = companyInput.value
@@ -46,34 +143,15 @@ saveButton.addEventListener('click', () => {
 
   clients.push(addClient)
   
+  displayClients()
 
-  table.innerHTML = `
-      <tr>
-        <th>ID</th>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
-        <th>Edit | Delete</th>
-      </tr>
-      `
-  var displayClients = clients.map((client, i) => {
-    return `
-          
-      <tr>
-        <td>${client.id}</td>
-        <td>${client.company}</td>
-        <td>${client.contact}</td>
-        <td>${client.country}</td>
-        <td><i id="editButton" class="fa-solid fa-pen"></i><i id="deleteButton" class="fa-solid fa-trash"></i></td>
-      </tr>
-    `
-  })
   
-  table.innerHTML += displayClients.join("")
-  modal.style.display = "none";
+}
 
 
-  var deleteButton = document.querySelectorAll('#deleteButton');
+/////////
+
+var deleteButton = document.querySelectorAll('#deleteButton');
   deleteButton.forEach((button) => {
     button.addEventListener('click', (e) => {
       const id = Number(e.target.parentElement.parentElement.firstChild.nextSibling.innerHTML)
@@ -108,25 +186,5 @@ saveButton.addEventListener('click', () => {
       table.innerHTML += displayFiltered.join("")
     })
   })
-  
-  
-  
-})
-
-
-
-
-
-
-
-/*
-
 */
-
-
-
-
-
-
-
 
