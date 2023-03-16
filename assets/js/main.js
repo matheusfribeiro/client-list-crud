bc8a133b5fc9d40ad19a6cc22d6b58b5c3689a10
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-03-16 19:00:13 -0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const Main = {
     this.bindEvents()
     
     
+    
   },
 
   cacheSelectors: function () {
@@ -17,7 +18,7 @@ const Main = {
     this.contactInput = document.querySelector('#contactName')
     this.countryInput = document.querySelector('#countryName')
     this.inputs = document.querySelectorAll('input')
-    this.removeButtons = document.querySelectorAll('#deleteButton')
+    //this.removeButtons = document.querySelectorAll('.fa-trash')
     this.clients = []
   },
 
@@ -32,9 +33,11 @@ const Main = {
 
     this.saveButton.addEventListener('click', this.Events.saveClient_click.bind(this))
 
+    /*
     this.removeButtons.forEach(function(button) {
       button.onclick = self.Events.removeButton_click
     })
+    */
   },
 
 
@@ -54,13 +57,16 @@ const Main = {
 
     saveClient_click: function () {
       const clientsScoped = this.clients
+      let id = crypto.randomUUID()
+      
+      
       
       companyName = this.companyInput.value
       contactName = this.contactInput.value
       countryName = this.countryInput.value
 
       const addClient = {
-        id: (clientsScoped.length + 1),
+        id: clientsScoped.length + 1,
         company: companyName,
         contact: contactName,
         country: countryName
@@ -69,38 +75,80 @@ const Main = {
       if (companyName && contactName && countryName) {
         clientsScoped.push(addClient)
         modal.style.display = "none";
+        this.displayClients()
       } else {
         alert('All fields are required')
       }
       
-      this.displayClients()
+      
     
     },
 
-    removeButton_click: function () {
-      alert('test for echo')
+    removeButton_click: function (id) {
+      clientsScoped = Main.clients
+      console.log(tablebody)
+
+      
+      for ( let i = 0; i < clientsScoped.length; i++) {
+        if (clientsScoped[i].id == id) {
+          clientsScoped.splice(i, 1)
+          tablebody.deleteRow(i)
+        }
+      }
+      
+    },
+    
+    editButton_click: function (data) {
+      Main.inputs.forEach(input => {
+        input.value = ''
+      })
+      modal.style.display = "block";
+      Main.companyInput.value = data.company
+      Main.contactInput.value = data.contact
+      Main.countryInput.value = data.company
+    },
+
+    editUpdate: function () {
+      
     }
   },
 
+
   displayClients: function () {
     clientsScoped = this.clients
-    this.tbody.innerHTML = null
-    
+    tablebody = this.tbody
+    tablebody.innerText = null
 
-    clientsScoped.map((client) => {
-      this.tbody.innerHTML += `
-        <tr>
-          <td>${client.id}</td>
-          <td>${client.company}</td>
-          <td>${client.contact}</td>
-          <td>${client.country}</td>
-          <td><i id="editButton" class="fa-solid fa-pen"></i><i id="deleteButton" class="fa-solid fa-trash"></i></td>
-        </tr>
-      `
+    for(let i = 0; i < clientsScoped.length; i++) {
+      let tr = tablebody.insertRow()
+
+      let td_id = tr.insertCell()
+      let td_company = tr.insertCell()
+      let td_contact = tr.insertCell()
+      let td_country = tr.insertCell()
+      let td_actions = tr.insertCell()
+
+      td_id.innerText = clientsScoped[i].id
+      td_company.innerText = clientsScoped[i].company
+      td_contact.innerText = clientsScoped[i].contact
+      td_country.innerText = clientsScoped[i].country
+
+      let imgEdit = document.createElement("i")
+      imgEdit.setAttribute("class", "fa-solid fa-pen")
+      imgEdit.setAttribute("onclick", `Main.Events.editButton_click(${JSON.stringify(clientsScoped[i])})`)
+      
+      
+      let imgDelete = document.createElement("i")
+      imgDelete.setAttribute("class", "fa-solid fa-trash")
+      imgDelete.setAttribute("onclick", `Main.Events.removeButton_click(${clientsScoped[i].id})`)
+      
+      td_actions.appendChild(imgEdit)
+      td_actions.appendChild(imgDelete)
+      
       console.log(clientsScoped)
-      this.cacheSelectors()
-      this.bindEvents()
-    }).join('')
+      
+      
+    }
   },
 
 
